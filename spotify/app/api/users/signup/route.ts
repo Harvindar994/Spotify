303d4a7f9b15e3a42ccfc4030321b7bc610f8ef5
Harvindar Singh <sjagjeet994@gmail.com>
2024-01-05 01:50:54 +0530

    created auth api

--- spotify/app/api/users/signup/route.ts
+++ spotify/app/api/users/signup/route.ts
@@ -1,16 +1,46 @@
 import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
 
 export async function POST(request: Request) {
     try {
         const body = await request.json();
 
+        const {name, email, password} = body;
+
+        const user = await prisma.user.findFirst({
+            where: {
+                email
+            }
+        })
+
+        if (user){
+            return NextResponse.json({
+                error: "User already exists"
+            }, {status: 400})
+        }
+        
+        // hashing the password.
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
         const newUser = await prisma.user.create({
-                            data: body,
+                            data: {
+                                name,
+                                email,
+                                password: hashedPassword
+                            }
                         });
                         
-        return Response.json(newUser);
+        return Response.json({
+            message: "User created successfully",
+            success: true,
+            savedUser: newUser
+        }, {
+            status: 200
+        });
 
-    } catch (error) {
-        return Response.json(error)
+    } catch (error: any) {
+        return Response.json({error: error.message}, {status: 500})
     }
 }
--- spotify/app/auth/signup/page.tsx
+++ spotify/app/auth/signup/page.tsx
@@ -11,18 +11,50 @@ import Link from 'next/link';
 import { MdLightMode } from "react-icons/md";
 import { MdDarkMode } from "react-icons/md";
 import { IconButton } from '@radix-ui/themes';
+import axios from 'axios';
+import { ColorRing } from 'react-loader-spinner';
 
 const Login = () => {
     const [currentPage, setCurrentPage] = useState("Home");
-    const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState("dark");
+    const [creatingAccount, setCreatingAccount] = useState(false);
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
 
     function changeTheme(){
         if (theme === "dark"){
-        setTheme("light");
+            setTheme("light");
         }
         else{
-        setTheme("dark");
+            setTheme("dark");
         }
+    }
+
+    function handleFormChanges(event: any){
+        const {name, value} = event.currentTarget;
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+
+    async function onFormSubmit(event: any){
+        if (creatingAccount){
+            return;
+        }
+        setCreatingAccount(true);
+        event.preventDefault();
+        console.log("working");
+        try {
+            const respose = await axios.post("/api/users/signup", user);
+            console.log(respose);
+        } catch (error) {
+            console.log(error);
+        }
+        setCreatingAccount(false);
     }
 
     return (
@@ -34,31 +66,40 @@ const Login = () => {
                     {theme == "dark" ? <MdLightMode />: <MdDarkMode />}
                 </IconButton>
             </div>
-            <div className={styles.loginForm}>
+            <form className={styles.loginForm} onSubmit={onFormSubmit}>
                 <div className={styles.title}>Sign up to start <br/> listening</div>
                 <div className={styles.fields}>
                     <div className={styles.field}>
                         <div className={styles.fieldName}>Name</div>
-                        <TextFieldInput placeholder="Name" size="3" className={styles.fieldInput}/>
+                        <TextFieldInput placeholder="Name" size="3" onChange={handleFormChanges} value={user.name} name='name' className={styles.fieldInput}/>
                     </div>
                     <div className={styles.field}>
                         <div className={styles.fieldName}>Email address</div>
-                        <TextFieldInput placeholder="Email or username" size="3" className={styles.fieldInput}/>
+                        <TextFieldInput placeholder="Email or username" size="3" onChange={handleFormChanges} value={user.email} name='email' className={styles.fieldInput}/>
                     </div>
                     <div className={styles.field}>
                         <div className={styles.fieldName}>Password</div>
-                        <TextFieldInput placeholder="Password" size="3" type='password' className={styles.fieldInput}/>
+                        <TextFieldInput placeholder="Password" size="3" onChange={handleFormChanges} value={user.password} type='password' name='password' className={styles.fieldInput}/>
                     </div>
                     <div className={styles.fieldRemember}>
                         <Checkbox/>
                         <p>I would prefer not to receive marketing messages from Spotify</p>
                     </div>
-                    <Button radius="full" variant="soft" size="3" className={styles.loginButton}>
-                        Sign up
+                    <Button radius="full" type='submit' variant="soft" size="3" className={styles.loginButton}>
+                        {creatingAccount ? 
+                            <ColorRing
+                            visible={true}
+                            height="30"
+                            width="30"
+                            ariaLabel="color-ring-loading"
+                            wrapperStyle={{}}
+                            wrapperClass="color-ring-wrapper"
+                            colors={['var(--gray-12)', 'var(--gray-12)', 'var(--gray-12)', 'var(--gray-12)', 'var(--gray-12)']}
+                            />: "Sign up"} 
                     </Button>
                     <Link href="/auth/login" className={styles.forgetPassword}>Already have an account? Log in here</Link>
                 </div>
-            </div>
+            </form>
             <div className={styles.shade}></div>
         </Theme>
     )
